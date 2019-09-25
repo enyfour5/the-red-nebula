450e1773042d0e5e8e13908878b09ae57d81b8c6
Rene Rosales Jr <enyfour5@gmail.com>
2019-09-25 04:19:21 +0000

    completed all of my express validation handlers in the route, and ready for client side error handling so that the user can see what mistake they have made when trying to register as a new user

--- routes/register.js
+++ routes/register.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var db = require('../models/db');
 var bodyParser = require('body-parser');
-const { check, validationResult } = require('express-validator');
+const { body, check, validationResult } = require('express-validator');
 const bcrypt = require('bcrypt');
 
 app.use(bodyParser.urlencoded({
@@ -15,8 +15,20 @@ router.get('/register', function(req, res, next) {
   res.render('register', { title: 'Register for a free account' });
 });
 
-router.post('/register', [
-check('username').not().isEmpty().withMessage('please fill out username')
+router.post('/register', [ body('confirm_password').custom((value, { req }) => {
+  if (value !== req.body.password) {
+    throw new Error('Password confirmation does not match password');
+    }
+    return true;
+}),
+  check('username').not().isEmpty().withMessage('please fill out username'),
+  check('username', 'Username can only contain letters, numbers, or underscores.').matches(/^[A-Za-z0-9_-]+$/, 'i'),
+  check('username', 'Username must be between 4-15 characters long.').isLength({min: 4, max: 14}),
+  check('email', 'The email you entered is invalid, please try again.').isEmail(),
+  check('email', 'Email address must be between 4-100 characters long, please try again.').isLength({min: 4, max: 100}),
+  check('password', 'Password must be between 8-100 characters long.').isLength({min: 8, max: 100}),
+  check('password', "Password must include one lowercase character, one uppercase character, a number, and a special character.").matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[^a-zA-Z0-9]).{8,}$/, "i"),
+  check('confirm_password', 'Password must be between 8-100 characters long.').isLength({min: 4, max: 100})
 ], function(req, res) {
 
   const errors = validationResult(req);
